1bcec54c7e1a5d7e2eba758b06a1827a5a58dd0e
Jannis Adamek <jannis.adamke@gmail.com>
2023-05-17 02:20:47 +0200

    Draw tile data as a texture.

--- src/video.rs
+++ src/video.rs
@@ -1,10 +1,17 @@
-use sdl2::{pixels::Color, rect::Rect, render::WindowCanvas, Sdl};
+use sdl2::{
+    pixels::{Color, PixelFormatEnum},
+    rect::Rect,
+    render::{Texture, TextureCreator, WindowCanvas},
+    video::WindowContext,
+    Sdl,
+};
 
 use crate::bus::Bus;
 
 pub struct Video<'a> {
     sdl_context: &'a Sdl,
     canvas: WindowCanvas,
+    tile_data_texture_creator: TextureCreator<WindowContext>,
     tile_data_canvas: WindowCanvas,
 }
 
@@ -33,9 +40,12 @@ impl Video<'_> {
             .build()
             .expect("could not make a canvas");
 
+        let tile_data_texture_creator = tile_data_canvas.texture_creator();
+
         return Video {
             sdl_context,
             canvas,
+            tile_data_texture_creator,
             tile_data_canvas,
         };
     }
@@ -66,23 +76,26 @@ impl Video<'_> {
     }
 
     pub fn draw_tile_data(&mut self, bus: &Bus) {
-        self.tile_data_canvas.clear();
         let mut addr = 0x8000;
-        let scale = 2;
-        for tile in 0..384 {
-            let start_x = (tile % 20) * 8;
-            let start_y = (tile / 20) * 8;
-            draw_tile(
-                bus,
-                &mut self.tile_data_canvas,
-                addr,
-                scale,
-                start_x,
-                start_y,
-            );
-            addr += 16;
-        }
-        println!("end drawing at {:x}", addr);
+        let mut texture = self
+            .tile_data_texture_creator
+            .create_texture_streaming(PixelFormatEnum::RGB24, 160, 160)
+            .expect("Couldn't create texture!");
+        texture
+            .with_lock(None, |buffer: &mut [u8], _| {
+                for tile in 0..384 {
+                    let start_x = (tile % 20) * 8;
+                    let start_y = (tile / 20) * 8;
+                    draw_tile_into_texture(bus, buffer, addr, start_x, start_y);
+                    addr += 16;
+                }
+            })
+            .expect("Couldn't draw into texture.");
+
+        self.tile_data_canvas.clear();
+        self.tile_data_canvas
+            .copy(&texture, None, None)
+            .expect("copy");
         self.tile_data_canvas.present();
     }
 }
@@ -133,5 +146,46 @@ fn draw_tile(
         //         8 * scale as u32,
         //     ))
         //     .unwrap();
+    }
+}
+
+fn draw_tile_into_texture(bus: &Bus, buffer: &mut [u8], addr: u16, start_x: i32, start_y: i32) {
+    let mut addr = addr;
+    for pixel_y in 0..8 {
+        let byte1 = bus.read(addr);
+        addr += 1;
+        let byte2 = bus.read(addr);
+        addr += 1;
+        for shift in (0..8).rev() {
+            let higher = ((byte1 >> shift) & 1) << 1;
+            let lower = (byte2 >> shift) & 1;
+            let color_id = higher | lower;
+            let pos_x = (7 - shift + (start_x as i32)) as usize;
+            let pos_y = (pixel_y + start_y) as usize;
+            let pos_buf = (pos_y * 160 + pos_x) * 3;
+            match color_id {
+                0 => {
+                    buffer[pos_buf] = 0xFF;
+                    buffer[pos_buf + 1] = 0xFF;
+                    buffer[pos_buf + 2] = 0xFF;
+                }
+                1 => {
+                    buffer[pos_buf] = 0x80;
+                    buffer[pos_buf + 1] = 0x80;
+                    buffer[pos_buf + 2] = 0x80;
+                }
+                2 => {
+                    buffer[pos_buf] = 0;
+                    buffer[pos_buf + 1] = 0xFF;
+                    buffer[pos_buf + 2] = 0xFF;
+                }
+                3 => {
+                    buffer[pos_buf] = 0;
+                    buffer[pos_buf + 1] = 0;
+                    buffer[pos_buf + 2] = 0;
+                }
+                _ => unreachable!(),
+            };
+        }
     }
 }
